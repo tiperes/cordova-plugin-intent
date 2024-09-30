fcce2798fa41847d58a5f67c74c96f4bedfb6f86
tiperes <66407614+tiperes@users.noreply.github.com>
2024-09-30 20:39:24 +0100

    Update CordovaPluginIntentFileProvider.java

--- src/android/CordovaPluginIntentFileProvider.java
+++ src/android/CordovaPluginIntentFileProvider.java
@@ -1,4 +1,4 @@
-package com.darryncampbell.cordova.plugin.intent;
+package com.tiperes.cordova.plugin.intent;
 
 public class CordovaPluginIntentFileProvider extends androidx.core.content.FileProvider {
 }
