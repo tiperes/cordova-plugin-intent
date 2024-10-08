f7acc4a1a5d899b019ff8962c10b176701b1a4dc
tiperes <66407614+tiperes@users.noreply.github.com>
2024-10-08 03:48:41 +0100

    Create after_plugin_add.js

---
+++ hooks/android/after_plugin_add.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require('path');
+const et = require('elementtree');
+
+function processCordovaVariables (context) {
+    const packagesToIncludeCSV = context.opts.cli_variables.CORDOVA_ANDROID_PACKAGES;
+    if (!packagesToIncludeCSV) {
+        console.log('CORDOVA_ANDROID_PACKAGES variable not provided.');
+        return;
+    }
+    
+    const androidPlatformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+    const androidManifestPath = path.join(androidPlatformRoot, 'app/src/main/AndroidManifest.xml');
+    
+    let androidManifestXml = et.parse(fs.readFileSync(androidManifestPath, 'utf-8'));
+    let androidManifestRoot = androidManifestXml.getroot();
+    let queriesXmlNode = androidManifestRoot.find('./queries');
+    if (queriesXmlNode == null) {
+        queriesXmlNode = et.fromstring('<queries/>');
+        androidManifestRoot.append(queriesXmlNode);
+    }
+    packagesToIncludeCSV.split(',').forEach(package => {
+        if (queriesXmlNode.find(`./package[android:name="${package}"]`) == null) {
+            queriesXmlNode.append(et.fromstring(`<package android:name="${package}"/>`));
+        }
+    });
+
+    fs.writeFileSync(androidManifestPath, androidManifestXml.write({ indent: 4 }), 'utf-8');
+}
+
+module.exports = function (context) {
+    processCordovaVariables(context);
+};
