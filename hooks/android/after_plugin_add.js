f1f1468892b84317e145653deb974fa37e2b4790
tiperes <66407614+tiperes@users.noreply.github.com>
2024-10-08 12:08:19 +0100

    Update after_plugin_add.js

--- hooks/android/after_plugin_add.js
+++ hooks/android/after_plugin_add.js
@@ -4,55 +4,55 @@ const et = require('elementtree');
 
 // Function to merge the manifests
 function mergeManifests(context) {
-	let removeDuplicates = false;
-	let xmlNodePath2ContentXml = [];
-	Object.keys(context.opts.cli_variables).forEach((name) => {
-		if (name == 'ANDROID_REMOVE_DUPLICATES') {
-			removeDuplicates = context.opts.cli_variables[name] == 'true';
-		} else {
-			xmlNodePath2ContentXml.push(name, context.opts.cli_variables[name]);
-		}
-	});
-	
-	if (xmlNodePath2ContentXml.length == 0) {
+    let removeDuplicates = false;
+    let xmlNodePath2ContentXml = [];
+    Object.keys(context.opts.cli_variables).forEach((name) => {
+        if (name == 'ANDROID_REMOVE_DUPLICATES') {
+            removeDuplicates = context.opts.cli_variables[name] == 'true';
+        } else {
+            xmlNodePath2ContentXml.push(name, context.opts.cli_variables[name]);
+        }
+    });
+    
+    if (xmlNodePath2ContentXml.length == 0) {
         console.log('No Manifest changes todo.');
         return;
     }
-	
-	const androidPlatformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-	const originalManifestPath = path.join(context.opts.projectRoot, 'app/src/main/AndroidManifest.xml');
+    
+    const androidPlatformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+    const originalManifestPath = path.join(context.opts.projectRoot, 'app/src/main/AndroidManifest.xml');
     if (!fs.existsSync(originalManifestPath)) {
         console.error('AndroidManifest.xml not found at:', originalManifestPath);
         return;
     }
-	
+    
     const originalManifestContent = fs.readFileSync(originalManifestPath, 'utf-8');
     const originalManifestTree = et.parse(originalManifestContent);
-	
-	xmlNodePath2ContentXml.forEach((nodePath) => {
-		// Start by removing duplicates
-		if (removeDuplicates) {
-			const parent = originalManifestTree.find(`{nodePath}/*`);
-			if (parent != null) {
-				var children = parent.getchildren();
-				for (let i = 0; i < children.length; i++) {
-					for (let j = i; j < children.length; j++) {
-						if (et.equalNodes(children[i], children[j])) {
-							originalManifestTree.remove(children[j]);
-							children.splice(j, 1);
-							break;
-						}
-					}
-				}
-			}
-		}
-		
-		// Append only if the node does not exist already
-		const nodeChildren = et.parse(`<root>{xmlNodePath2ContentXml[nodePath]}</root>`).getchildren();
-		if (nodeChildren.length > 0) {
-			et.graftXML(originalManifestTree, nodeContent.getchildren(), nodePath);
-		}
-	});
+    
+    xmlNodePath2ContentXml.forEach((nodePath) => {
+        // Start by removing duplicates
+        if (removeDuplicates) {
+            const parent = originalManifestTree.find(`{nodePath}/*`);
+            if (parent != null) {
+                var children = parent.getchildren();
+                for (let i = 0; i < children.length; i++) {
+                    for (let j = i; j < children.length; j++) {
+                        if (et.equalNodes(children[i], children[j])) {
+                            originalManifestTree.remove(children[j]);
+                            children.splice(j, 1);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        // Append only if the node does not exist already
+        const nodeChildren = et.parse(`<root>{xmlNodePath2ContentXml[nodePath]}</root>`).getRoot().getchildren();
+        if (nodeChildren.length > 0) {
+            et.graftXML(originalManifestTree, nodeChildren, nodePath);
+        }
+    });
 
     // Write the updated manifest back to the original file
     fs.writeFileSync(originalManifestPath, originalManifestTree.write({ indent: 4 }), 'utf-8');
