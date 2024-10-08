24ba6f374cd8ddbba2a59db65cd6060e4693f7b8
tiperes <66407614+tiperes@users.noreply.github.com>
2024-10-08 14:48:30 +0100

    Update after_plugin_add.js

--- hooks/android/after_plugin_add.js
+++ hooks/android/after_plugin_add.js
@@ -1,87 +1,173 @@
-const fs = require("fs");
+const q = require('q');
 const path = require('path');
-const et = require('elementtree');
+const fs = require('fs');
+const xml2js = require('xml2js');
 
-function processCordovaVariables (context) {
-    // Base paths
-    const androidPlatformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-    const androidManifestPath = path.join(androidPlatformRoot, 'app/src/main/AndroidManifest.xml');
-    
-    // Init AndroidManifest control
-    console.log('processCordovaVariables - 1');
-    const androidManifestXml = et.parse(fs.readFileSync(androidManifestPath, 'utf-8'));
-    const androidManifestNode = androidManifestXml.getroot();
-    let manifestUpdated = false;
-    console.log('processCordovaVariables - 2');
-    // Queries permissions
+module.exports = function (context) {
+    // Check if any expected client variable is defined
     const packagesToIncludeCSV = context.opts.cli_variables.ANDROID_QUERIES_PACKAGES;
     const actionsToIncludeCSV = context.opts.cli_variables.ANDROID_QUERIES_ACTIONS;
-    if (!packagesToIncludeCSV || !actionsToIncludeCSV) {
-        // If missing, add queries node
-        console.log('processCordovaVariables - 3');
-        let queriestNode = androidManifestNode.find('./queries');
-        if (queriestNode == null) {
-            console.log('processCordovaVariables - 4');
-            queriestNode = et.Element('queries');
-            androidManifestNode.append(queriestNode);
-        }
-        console.log('processCordovaVariables - 5');
-        if (!packagesToIncludeCSV) {
-            packagesToIncludeCSV.split(',').forEach(package => {
-                console.log('processCordovaVariables - 6');
-                if (queriestNode.find(`./package[android:name="${package}"]`) == null) {
-                    console.log('processCordovaVariables - 7');
-                    queriestNode.append(et.Element('package', {"android:name": package}));
-                    manifestUpdated = true;
-                }
-            });
-            console.log('ANDROID_QUERIES_PACKAGES configured: ' + packagesToIncludeCSV);
-        } else {
-            console.log('ANDROID_QUERIES_PACKAGES not provided.');
-        }
-        
-        if (!actionsToIncludeCSV) {
-            actionsToIncludeCSV.split(',').forEach(actionMimeType => {
-                const actionMimeTypeSplit = actionMimeType.split('|');
-                const action = actionMimeTypeSplit[0];
-                const mimeType = actionMimeTypeSplit.length == 1 ? '' : actionMimeTypeSplit[1];
-                    
-                if (queriestNode.find(`./intent/action[android:name="${action}"]`) == null) {
-                    let intentNode = et.Element('intent');
-                    intentNode.append(et.Element('action', { "android:name": action }));
-                    if (mimeType) {
-                        intentNode.append(et.Element('data', { "android:scheme": "content", "android:mimeType": mimeType }));
-                    }
-                    queriestNode.append(intentNode);
-                    manifestUpdated = true;
-                }
-            });
-            console.log('ANDROID_QUERIES_ACTIONS configured: ' + actionsToIncludeCSV);
-        } else {
-            console.log('ANDROID_QUERIES_ACTIONS not provided.');
-        }
-    }
-    
     const permissionsToIncludeCSV = context.opts.cli_variables.ANDROID_USES_PERMISSIONS;
-    if (!permissionsToIncludeCSV) {
-        permissionsToIncludeCSV.split(',').forEach(permission => {
-            if (androidManifestNode.find(`./uses-permission[android:name="${permission}"]`) == null) {
-                androidManifestNode.append(et.Element('uses-permission', { "android:name": permission }));
-                manifestUpdated = true;
-            }
-        });
+    
+    if (packagesToIncludeCSV) {
+        console.log('ANDROID_QUERIES_PACKAGES configured: ' + packagesToIncludeCSV);
+    } else {
+        console.log('ANDROID_QUERIES_PACKAGES not provided.');
+    }
+    if (actionsToIncludeCSV) {
+        console.log('ANDROID_QUERIES_ACTIONS configured: ' + actionsToIncludeCSV);
+    } else {
+        console.log('ANDROID_QUERIES_ACTIONS not provided.');
+    }
+    if (permissionsToIncludeCSV) {
         console.log('ANDROID_USES_PERMISSIONS configured: ' + permissionsToIncludeCSV);
     } else {
         console.log('ANDROID_USES_PERMISSIONS not provided.');
     }
-    
-    // Save updated AndroidManifest
-    if (manifestUpdated) {
-        fs.writeFileSync(androidManifestPath, strings.write({ indent: 4 }), 'utf-8');
-        console.log('AndroidManifest.xml updated with requested permissions');
+    if (!packagesToIncludeCSV && !actionsToIncludeCSV && !permissionsToIncludeCSV) {
+        return;
     }
-}
+    
+    const deferral = new q.defer();
 
-module.exports = function (context) {
-    processCordovaVariables(context);
+    // Path to the AndroidManifest.xml file
+    const androidPlatformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+    const androidManifestPath = path.join(androidPlatformRoot, 'app/src/main/AndroidManifest.xml');
+    // Read AndroidManifest.xml
+    fs.readFile(androidManifestPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Failed to read AndroidManifest.xml:', err);
+            deferral.reject();
+            return;
+        }
+
+        // Parse the XML to JavaScript object
+        xml2js.parseString(data, (err, result) => {
+            if (err) {
+                console.error('Failed to parse AndroidManifest.xml:', err);
+                deferral.reject();
+                return;
+            }
+            
+            if (packagesToIncludeCSV || actionsToIncludeCSV) {
+                // Check if <queries> element already exists
+                let queriesNode = result.['manifest']['queries'];
+                if (!queriesNode) {
+                    // Append <queries> element to the root
+                    queriesNode = [];
+                    result.['manifest']['queries'] = queriesNode;
+                } else {
+                    console.log('<queries> node already exists.');
+                }
+            
+                // Process the required packages
+                if (packagesToIncludeCSV) {
+                    packagesToIncludeCSV.split(',').forEach(packageName => {
+                        let packageExists = false;
+                        queriesNode.forEach(query => {
+                            if (query.package) {
+                                query.package.forEach(package => {
+                                    if (package.$ && package.$['android:name'] == packageName) {
+                                        packageExists = true;
+                                    }
+                                });
+                            }
+                            
+                        });
+                        if (!packageExists) {
+                            queriesNode.push({
+                                package: [{ $: { 'android:name': packageName } }]
+                            });
+                            console.log(`Package '${packageName}' added to <queries> node.`);
+                        } else {
+                            console.log(`Package '${packageName}' already exists.`);
+                        }
+                    });
+                }
+                
+                // Process the required actions
+                if (actionsToIncludeCSV) {
+                    actionsToIncludeCSV.split(',').forEach(actionNameMimeType => {
+                        const actionNameMimeTypeSplit = actionNameMimeType.split('|');
+                        const actionName = actionNameMimeTypeSplit[0];
+                        const mimeType = actionNameMimeTypeSplit.length == 1 ? '' : actionNameMimeTypeSplit[1];
+                        
+                        let actionExists = false;
+                        queriesNode.forEach(query => {
+                            if (query.intent) {
+                                query.intent.forEach(intent => {
+                                    if (intent.action) {
+                                        intent.action.forEach(action => {
+                                            if (action.$ && action.$['android:name'] === actionName') {
+                                                actionExists = true;
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                        if (!actionExists) {
+                            const intentNode = [{
+                                action: [{ $: { 'android:name': actionName } }]
+                            }];
+                            if (mimeType) {
+                                intentNode.push({
+                                    data: [{ $: { 'android:scheme': 'content', 'android:mimeType': mimeType } }]
+                                });
+                            }
+                            queriesNode.push({
+                                intent: intentNode
+                            });
+                            console.log(`Intent action '${actionName}' with mime type '${mimeType}' added <queries> to node.`);
+                        } else {
+                            console.log(`Intent action '${actionName}' with mime type '${mimeType}' already exists.`);
+                        }
+                    });
+                }
+            }
+            
+            // Process the required packages
+            if (permissionsToIncludeCSV) {
+                permissionsToIncludeCSV.split(',').forEach(permissionName => {
+                    let permissionsNode = result.['manifest']['uses-permission'];
+                    if (!permissionsNode) {
+                        // Append <uses-permission> element to the root
+                        permissionsNode = [];
+                        result['manifest']['uses-permission'] = queriesNode;
+                    } else {
+                        console.log('<uses-permission> node already exists.');
+                    }
+                    
+                    permissionsNode.forEach(permission => {
+                        if (permission.$ && permission.$['android:name'] == permissionName) {
+                            permissionExists = true;
+                        }
+                    });
+                    if (!permissionExists) {
+                        permissionsNode.push({ $: { 'android:name': permissionName } });
+                        console.log(`Uses Permission '${permissionName}' added <queries> to node.`);
+                    } else {
+                        console.log(`Uses Permission '${permissionName}' already exists.`);
+                    }
+                });
+            }
+
+            // Convert the updated JavaScript object back to XML
+            const builder = new xml2js.Builder();
+            const updatedManifest = builder.buildObject(result);
+
+            // Write the updated XML back to AndroidManifest.xml
+            fs.writeFile(androidManifestPath, updatedManifest, 'utf8', (err) => {
+                if (err) {
+                    console.error('Failed to write AndroidManifest.xml:', err);
+                    deferral.reject();
+                } else {
+                    console.log('Successfully updated AndroidManifest.xml with <queries> node.');
+                    deferral.resolve();
+                }
+            });
+        });
+    });
+
+    return deferral.promise;
 };
