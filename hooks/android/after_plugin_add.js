d231859d0971a50b57447b13791bd63cae1222a2
tiperes <66407614+tiperes@users.noreply.github.com>
2024-10-08 05:41:38 +0100

    Update after_plugin_add.js

--- hooks/android/after_plugin_add.js
+++ hooks/android/after_plugin_add.js
@@ -1,21 +1,20 @@
+const q = require('q');
 const fs = require('fs');
 const path = require('path');
 const xml2js = require('xml2js');
 
 module.exports = function (context) {
-	// Check if any expected client variable is defined
-	const packagesToIncludeCSV = context.opts.cli_variables.ANDROID_QUERIES_PACKAGES;
-	if (!packagesToIncludeCSV) {
+    // Check if any expected client variable is defined
+    const packagesToIncludeCSV = context.opts.cli_variables.ANDROID_QUERIES_PACKAGES;
+    if (!packagesToIncludeCSV) {
         console.log('ANDROID_QUERIES_PACKAGES variable not provided.');
-		return;
+        return;
     }
-	
-    const Q = context.requireCordovaModule('q');
-    const deferral = new Q.defer();
+    
+    const deferral = new q.defer();
 
     // Path to the AndroidManifest.xml file
     const manifestPath = path.join(context.opts.projectRoot, 'platforms', 'android', 'app', 'src', 'main', 'AndroidManifest.xml');
-
     // Read AndroidManifest.xml
     fs.readFile(manifestPath, 'utf8', (err, data) => {
         if (err) {
@@ -33,30 +32,30 @@ module.exports = function (context) {
             }
 
             // Check if <queries> element already exists
-			let queriesNode = result.manifest.queries;
+            let queriesNode = result.manifest.queries;
             if (!queriesNode) {
                 // Append <queries> element to the root
-				queriesNode = [];
+                queriesNode = [];
                 result.manifest.queries = queriesNode;
             } else {
                 console.log('<queries> node already exists.');
             }
-			
-			// Process the required packages
-			packagesToIncludeCSV.split(',').forEach(packageName => {
-				let packageExists = false;
-				queriesNode.forEach(query => {
-					if (query.package && query.package.$ && query.package.$['android:name'] == packageName) {
-						packageExists = true;
-					}
-				});
-				if (!packageExists) {
-					queriesNode.push({ package: { $: { 'android:name': packageName } } });
-					console.log('Added <package android:name="android.intent.action.VIEW" /> to <queries> node.');
-				} else {
-					console.log('<package android:name="android.intent.action.VIEW" /> already exists.');
-				}
-			});
+            
+            // Process the required packages
+            packagesToIncludeCSV.split(',').forEach(packageName => {
+                let packageExists = false;
+                queriesNode.forEach(query => {
+                    if (query.package && query.package.$ && query.package.$['android:name'] == packageName) {
+                        packageExists = true;
+                    }
+                });
+                if (!packageExists) {
+                    queriesNode.push({ package: { $: { 'android:name': packageName } } });
+                    console.log('Added <package android:name="android.intent.action.VIEW" /> to <queries> node.');
+                } else {
+                    console.log('<package android:name="android.intent.action.VIEW" /> already exists.');
+                }
+            });
 
             // Convert the updated JavaScript object back to XML
             const builder = new xml2js.Builder();
