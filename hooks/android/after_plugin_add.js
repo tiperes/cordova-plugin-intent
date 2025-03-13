c48fd297402a894f04e87f9498d40b41aa47b5f2
tiperes <66407614+tiperes@users.noreply.github.com>
2025-03-13 20:21:57 +0000

    Update after_plugin_add.js
    Using a diferent approach to process manifest manipulation using an external rest service

--- hooks/android/after_plugin_add.js
+++ hooks/android/after_plugin_add.js
@@ -1,174 +1,79 @@
-const q = require('q');
 const path = require('path');
 const fs = require('fs');
-const xml2js = require('xml2js');
+const axios = require('axios');
 
-module.exports = function (context) {
-    // Check if any expected client variable is defined
-    const packagesToIncludeCSV = context.opts.cli_variables.ANDROID_QUERIES_PACKAGES;
-    const actionsToIncludeCSV = context.opts.cli_variables.ANDROID_QUERIES_ACTIONS;
-    const permissionsToIncludeCSV = context.opts.cli_variables.ANDROID_USES_PERMISSIONS;
-    
-    if (packagesToIncludeCSV) {
-        console.log('ANDROID_QUERIES_PACKAGES configured: ' + packagesToIncludeCSV);
-    } else {
-        console.log('ANDROID_QUERIES_PACKAGES not provided.');
-    }
-    if (actionsToIncludeCSV) {
-        console.log('ANDROID_QUERIES_ACTIONS configured: ' + actionsToIncludeCSV);
-    } else {
-        console.log('ANDROID_QUERIES_ACTIONS not provided.');
-    }
-    if (permissionsToIncludeCSV) {
-        console.log('ANDROID_USES_PERMISSIONS configured: ' + permissionsToIncludeCSV);
-    } else {
-        console.log('ANDROID_USES_PERMISSIONS not provided.');
-    }
-    if (!packagesToIncludeCSV && !actionsToIncludeCSV && !permissionsToIncludeCSV) {
-        return;
-    }
-    
-    const deferral = new q.defer();
-
-    // Path to the AndroidManifest.xml file
-    const androidPlatformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-    const androidManifestPath = path.join(androidPlatformRoot, 'app/src/main/AndroidManifest.xml');
-    // Read AndroidManifest.xml
-    fs.readFile(androidManifestPath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Failed to read AndroidManifest.xml:', err);
-            deferral.reject();
-            return;
-        }
-
-        // Parse the XML to JavaScript object
-        xml2js.parseString(data, (err, result) => {
-            if (err) {
-                console.error('Failed to parse AndroidManifest.xml:', err);
-                deferral.reject();
-                return;
-            }
-            
-            if (packagesToIncludeCSV || actionsToIncludeCSV) {
-                // Check if <queries> element already exists
-                let queriesNode = result['manifest']['queries'];
-                if (!queriesNode) {
-                    // Append <queries> element to the root
-                    queriesNode = [];
-                    result['manifest']['queries'] = queriesNode;
-                } else {
-                    console.log('<queries> node already exists.');
-                }
-            
-                // Process the required packages
-                if (packagesToIncludeCSV) {
-                    packagesToIncludeCSV.split(',').forEach(packageName => {
-                        let packageExists = false;
-                        queriesNode.forEach(query => {
-                            if (query.package) {
-                                query.package.forEach(package => {
-                                    if (package.$ && package.$['android:name'] == packageName) {
-                                        packageExists = true;
-                                    }
-                                });
-                            }
-                            
-                        });
-                        if (!packageExists) {
-                            queriesNode.push({
-                                package: [{ $: { 'android:name': packageName } }]
-                            });
-                            console.log(`Package '${packageName}' added to <queries> node.`);
-                        } else {
-                            console.log(`Package '${packageName}' already exists.`);
-                        }
-                    });
-                }
-                
-                // Process the required actions
-                if (actionsToIncludeCSV) {
-                    actionsToIncludeCSV.split(',').forEach(actionNameMimeType => {
-                        const actionNameMimeTypeSplit = actionNameMimeType.split('|');
-                        const actionName = actionNameMimeTypeSplit[0];
-                        const mimeType = actionNameMimeTypeSplit.length == 1 ? '' : actionNameMimeTypeSplit[1];
-                        
-                        let actionExists = false;
-                        queriesNode.forEach(query => {
-                            if (query.intent) {
-                                query.intent.forEach(intent => {
-                                    if (intent.action) {
-                                        intent.action.forEach(action => {
-                                            if (action.$ && action.$['android:name'] === actionName) {
-                                                actionExists = true;
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                        if (!actionExists) {
-                            const intentNode = [{
-                                action: [{ $: { 'android:name': actionName } }]
-                            }];
-                            if (mimeType) {
-                                intentNode.push({
-                                    data: [{ $: { 'android:scheme': 'content', 'android:mimeType': mimeType } }]
-                                });
-                            }
-                            queriesNode.push({
-                                intent: intentNode
-                            });
-                            console.log(`Intent action '${actionName}' with mime type '${mimeType}' added <queries> to node.`);
-                        } else {
-                            console.log(`Intent action '${actionName}' with mime type '${mimeType}' already exists.`);
-                        }
-                    });
-                }
-            }
-            
-            // Process the required packages
-            if (permissionsToIncludeCSV) {
-                permissionsToIncludeCSV.split(',').forEach(permissionName => {
-                    let permissionsNode = result['manifest']['uses-permission'];
-                    if (!permissionsNode) {
-                        // Append <uses-permission> element to the root
-                        permissionsNode = [];
-                        result['manifest']['uses-permission'] = permissionsNode;
-                    } else {
-                        console.log('<uses-permission> node already exists.');
-                    }
-
-                    let permissionExists = false;
-                    permissionsNode.forEach(permission => {
-                        if (permission.$ && permission.$['android:name'] == permissionName) {
-                            permissionExists = true;
-                        }
-                    });
-                    if (!permissionExists) {
-                        permissionsNode.push({ $: { 'android:name': permissionName } });
-                        console.log(`Uses Permission '${permissionName}' added <queries> to node.`);
-                    } else {
-                        console.log(`Uses Permission '${permissionName}' already exists.`);
-                    }
-                });
-            }
-
-            // Convert the updated JavaScript object back to XML
-            const builder = new xml2js.Builder();
-            const updatedManifest = builder.buildObject(result);
-
-            // Write the updated XML back to AndroidManifest.xml
-            fs.writeFile(androidManifestPath, updatedManifest, 'utf8', (err) => {
-                if (err) {
-                    console.error('Failed to write AndroidManifest.xml:', err);
-                    deferral.reject();
-                } else {
-                    console.log('Successfully updated AndroidManifest.xml with <queries> node.');
-                    deferral.resolve();
-                }
-            });
-        });
-    });
-
-    return deferral.promise;
-};
+module.exports = async function (context) {
+	try {
+		const apiUrl = context.opts.cli_variables['ANDROID_PROCESS_MANIFEST_API'];
+		const apiAuthorization = context.opts.cli_variables['ANDROID_PROCESS_MANIFEST_AUTHORIZATION'];
+		if (!apiUrl) {
+			console.log('ANDROID_PROCESS_MANIFEST: Not required');
+			return;
+		}
+		
+		console.log('ANDROID_PROCESS_MANIFEST: Required using ' + apiUrl);
+		
+		// Path to the AndroidManifest.xml file
+		const androidPlatformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+		const androidManifestPath = path.join(androidPlatformRoot, 'app/src/main/AndroidManifest.xml');
+		let manifestXml = null;
+		
+		// Read AndroidManifest.xml
+		try {
+			manifestXml = await fs.promises.readFile(androidManifestPath, 'utf8');
+		}
+		catch (err) {
+			throw new Error('Failed to read AndroidManifest.xml', { cause: err });
+		}
+		
+		// Prepare Request Header
+		const apiConfig = {
+			headers: {
+				'Content-Type': 'application/json',
+				...(apiAuthorization ? { 'Authorization': apiAuthorization } : {}) // Only add if defined
+			}
+		};
+		
+		// Prepare Request Body
+		const apiData = {
+			timestamp: new Date().toISOString(),
+			plugin: context.opts.plugin.id,  // Plugin ID being added
+			platform: context.opts.platforms, // Platforms affected
+			manifestXml: manifestXml
+		};
+		
+		// Call the API to configure the manifest
+		let apiResponse;
+		try {
+			apiResponse = await axios.post(apiUrl, apiData, apiConfig);
+		} catch (err) {
+			let serializedError = {
+				message: err.message,
+				response: err.response ? {
+					status: err.response.status,
+					statusText: err.response.statusText,
+					data: err.response.data
+				} : undefined
+			};
+			throw new Error('Failed to call the API', { cause: serializedError });
+		}
+		
+		// If the API modified the manifest, write it back
+        if (apiResponse.data && apiResponse.data !== manifestXml) {
+			// Write AndroidManifest.xml
+			try {
+				await fs.promises.writeFile(androidManifestPath, apiResponse.data, 'utf8');
+				console.log('ANDROID_PROCESS_MANIFEST: Success, and the manifest has been modified');
+			}
+			catch (err) {
+				throw new Error('Failed to write AndroidManifest.xml', { cause: err });
+			}
+		}
+		else
+			console.log('ANDROID_PROCESS_MANIFEST: Success, and manifest has been validated');
+	}
+	catch (err) {
+		console.error('ANDROID_PROCESS_MANIFEST: Fail', err);
+		throw new Error('ANDROID_PROCESS_MANIFEST: Fail', { cause: err });
+	}
+}
